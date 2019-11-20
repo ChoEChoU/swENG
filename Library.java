fd09b9de92d928eaf865d5f8ac88301ee8cdf3c6
Lim Mintaek <lmtna99@gmail.com>
2019-11-20 19:11:19 +0900

    Modify Library Class

--- Library.java
+++ Library.java
@@ -7,27 +7,16 @@
  */
 public class Library
 {
-    // 인스턴스 변수 - 다음의 예제를 사용자에 맞게 바꾸십시오
-    private int x;
-
-    /**
-     * Library 클래스의 객체 생성자
-     */
-    public Library()
-    {
-        // 인스턴스 변수의 초기화
-        x = 0;
+    private String name; // 도서관은 하나의 이름을 가진다.
+    public Library(String name){
+        
     }
-
-    /**
-     * 예제 메소드 - 이 주석을 사용자에 맞게 바꾸십시오
-     * 
-     * @param  y   메소드의 예제 매개변수
-     * @return     x 더하기 y 
-     */
-    public int sampleMethod(int y)
-    {
-        // 여기에 코드를 작성하십시오.
-        return x + y;
+    public void registerOneBorrower(String name){ // 동명이인을 확인한다, new borrower(name)
+    
     }
+    public void registerOneBook(){}
+    public void displayBooksForLoan(){}
+    public void displayBooksOnLoan(){}
+    public void LendOneBook(){}
+    public void ReturnOneBook(){}
 }
