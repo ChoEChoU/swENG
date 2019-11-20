b3ae4e776e8e2c80bbcc809ada358591f0b5ea79
Lim Mintaek <lmtna99@gmail.com>
2019-11-20 19:24:40 +0900

    Borrower 생성자 수정과 Library 메소드 작성

--- Library.java
+++ Library.java
@@ -1,4 +1,4 @@
-
+import java.uitl.HashSet;
 /**
  * 여기에 Library 클래스 설명을 작성하십시오.
  * 
@@ -8,11 +8,14 @@
 public class Library
 {
     private String name; // 도서관은 하나의 이름을 가진다.
+    private HashSet <Borrower> registeredBorrowers;
     public Library(String name){
-        
+        this.name = name;
+        registeredBorrowers = new HashSet <Borrower> ();
     }
     public void registerOneBorrower(String name){ // 동명이인을 확인한다, new borrower(name)
-    
+        Borrower borrower = new Borrower(name);
+        registeredBorrowers.add(borrower);
     }
     public void registerOneBook(){}
     public void displayBooksForLoan(){}
