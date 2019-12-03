d61cb895461f967069cb4cc76e2a2231c427b31d
YUNJAEGOONER <coygyj@gmail.com>
2019-12-03 17:52:39 +0900

    MyFrame 작성, uc 6개에 해당하는 버튼을 만들고 프레임에 부착

--- MyFrame.java
+++ MyFrame.java
@@ -1,33 +1,35 @@
-
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 /**
  * Write a description of class MyFrame here.
  *
  * @author (your name)
  * @version (a version number or a date)
  */
-public class MyFrame
+public class MyFrame extends JFrame implements ActionListener 
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class MyFrame
-     */
-    public MyFrame()
-    {
-        // initialise instance variables
-        x = 0;
+    public MyFrame(){
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(900, 450);
+        this.setLayout(new BorderLayout());
+        JPanel mainpanel = new JPanel();
+        JButton btn_lend = new JButton("대출");
+        JButton btn_return = new JButton("반납");
+        JButton btn_newbook = new JButton("신규 도서 등록");
+        JButton btn_newborrower = new JButton("신규 이용자 등록");
+        JButton btn_displayon = new JButton("대출 가능한 책 조회");
+        JButton btn_displayfor = new JButton("대출 중인 책 조회");
+        mainpanel.add(btn_lend); 
+        mainpanel.add(btn_return); 
+        mainpanel.add(btn_newbook); 
+        mainpanel.add(btn_newborrower); 
+        mainpanel.add(btn_displayon); 
+        mainpanel.add(btn_displayfor);
+        this.add(mainpanel, BorderLayout.NORTH);
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public void actionPerformed(ActionEvent e){
     }
 }
